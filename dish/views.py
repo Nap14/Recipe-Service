8ed1207b14b5b91dc5e232abe114ffb6c72aaa02
Andrii Nykolenko <nikolandre14@gmail.com>
2022-11-30 14:15:08 +0200

    Home page path was created with new urls paht, index view, index templates

--- dish/views.py
+++ dish/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Chef, Dish, DishType
 
-# Create your views here.
+
+def index(request):
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+    context = {
+        "chefs": Chef.objects.count(),
+        "dishes": Dish.objects.count(),
+        "types": DishType.objects.count(),
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "dish/index.html", context=context)
