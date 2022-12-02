302deae524dbd2d7e13e6b2f984722495eb93ccc
Andrii Nykolenko <nikolandre14@gmail.com>
2022-12-02 14:47:44 +0200

    Create form.py with change in settengs

--- RecipeService/settings.py
+++ RecipeService/settings.py
@@ -44,8 +44,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
+    "crispy_forms",
     "dish",
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
---
+++ dish/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from dish.models import Dish, Product
+
+
+
